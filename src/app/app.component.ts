6491682499a6ba7c9ace4b0ac6b73c3d9e28672e
Danilo Sanchez <dsanchez@apexsystems.com>
2024-03-20 11:50:02 -0600

    cambios directivas de atributos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HomeworkAngularDs';
+  value='true';
+  active = true; 
+  changeColor() { 
+      this.active = !this.active; 
+      if (!this.active) { 
+          this.value = 'false'; 
+      } else { 
+          this.value = 'true'; 
+      } 
+  } 
 }
