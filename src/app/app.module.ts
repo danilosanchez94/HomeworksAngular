3eef71ff233630b5e11cc00b2f717b3153f5cc9d
Danilo Sanchez <dsanchez@apexsystems.com>
2024-04-02 17:08:09 -0600

    cambios para tarea 5 de modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DirectivasAtributosComponent } from './components/directivas-atributos/directivas-atributos.component';
+import { MyFirstModuleModule } from './my-first-module/my-first-module.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { DirectivasAtributosComponent } from './components/directivas-atributos/
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MyFirstModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
