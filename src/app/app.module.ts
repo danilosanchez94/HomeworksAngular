bf028191f52b154bd9e70d8f130f23e4c789c64c
Danilo Sanchez <dsanchez@apexsystems.com>
2024-04-03 16:59:41 -0600

    cambios para pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DirectivasAtributosComponent } from './components/directivas-atributos/directivas-atributos.component';
+import { PipesComponent } from './components/pipes/pipes.component';
+import { CurrencyPipe } from '@angular/common';
+import { UppercasecustomPipe } from './uppercasecustom.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DirectivasAtributosComponent
+    DirectivasAtributosComponent,
+    PipesComponent,
+    UppercasecustomPipe,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [CurrencyPipe, UppercasecustomPipe],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
