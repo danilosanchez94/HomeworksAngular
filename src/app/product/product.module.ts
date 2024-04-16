f91755fdc532f9b7a37fdfd7dbd063dd53dbac2c
Danilo Sanchez <dsanchez@apexsystems.com>
2024-04-16 16:20:03 -0600

    cambios para paginacion

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -5,12 +5,14 @@ import { ProductRoutingModule } from './product-routing.module';
 import { ProductListComponent } from './product-list/product-list.component';
 import { AngularmaterialModule } from '../angularmaterial.module';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { PaginationComponent } from './pagination/pagination.component';
 
 
 @NgModule({
   declarations: [
     ProductListComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    PaginationComponent
   ],
   imports: [
     CommonModule,
